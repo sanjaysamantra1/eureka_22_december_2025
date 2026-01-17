3492a074558aa30f4c1437da19bf91a8db15bea2
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-01-17 21:48:20 +0530

    data binding

---
+++ Angular/project1/src/app/components/databinding/databinding.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-databinding',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './databinding.html',
+  styleUrl: './databinding.css',
+})
+export class Databinding {
+  userName: string = 'Virat Kohli';
+  img_url: string = 'https://www.wisden.com/static-assets/images/players/3993.png?v=23.77';
+  flag: boolean = true;
+  max_length: number = 50;
+
+  toggleFlag() {
+    this.flag = !this.flag;
+  }
+}
