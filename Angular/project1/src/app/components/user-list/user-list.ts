d5eb0b62f97f84bb8fcf739e707cb3a51d4299a5
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-01-21 23:28:10 +0530

    user list

--- Angular/project1/src/app/components/user-list/user-list.ts
+++ Angular/project1/src/app/components/user-list/user-list.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import userData from './user_data';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { NgxPaginationModule } from 'ngx-pagination';
+import Swal from 'sweetalert2';
+import Snackbar from 'awesome-snackbar';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+    NgxPaginationModule
+  ],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
 export class UserList {
   userArr = userData;
+  faPhone = faPhone;
+  p = 1;
+
+  openSweetAlert() {
+    // Swal.fire('Good job!', 'You clicked the button!', 'success');
+    Swal.fire({ title: "The Internet?", text: "That thing is still around?", icon: "question", timer: 5000, draggable: true });
+  }
+  openSnackBar() {
+    new Snackbar('Helloooo, Good Morning',
+      { position: 'top-center', theme: 'light', timeout: 5000, actionText: 'X' }
+    );
+  }
 }
