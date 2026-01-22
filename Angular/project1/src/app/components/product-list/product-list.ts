dd0d5e584e93b0f822d5522fee0f5c1cd25e4415
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-01-22 22:06:00 +0530

    custom directives

--- Angular/project1/src/app/components/product-list/product-list.ts
+++ Angular/project1/src/app/components/product-list/product-list.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import * as data from './product_data.json';
+import { CharOnly } from '../../custom-directives/char-only';
+import { AlphaNumeric } from '../../custom-directives/alpha-numeric';
+import { DisableCopyPaste } from '../../custom-directives/disable-copy-paste';
+import { Zoomin } from '../../custom-directives/zoomin';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [
+    CharOnly,
+    AlphaNumeric,
+    DisableCopyPaste,
+    Zoomin
+  ],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
---
+++ Angular/project1/src/app/custom-directives/char-only.spec.ts
@@ -0,0 +1,8 @@
+import { CharOnly } from './char-only';
+
+describe('CharOnly', () => {
+  it('should create an instance', () => {
+    const directive = new CharOnly();
+    expect(directive).toBeTruthy();
+  });
+});
