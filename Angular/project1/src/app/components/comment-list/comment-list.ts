0386084d4908403b0ff360f656921eb7a673a69e
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-01-28 22:13:49 +0530

    httpclient with signal

--- Angular/project1/src/app/components/comment-list/comment-list.ts
+++ Angular/project1/src/app/components/comment-list/comment-list.ts
@@ -1,5 +1,5 @@
 import { CommentService } from './../../services/comment-service';
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-comment-list',
@@ -9,11 +9,15 @@ import { Component, inject, signal } from '@angular/core';
 })
 export class CommentList {
   commentService = inject(CommentService);
+  /* changeDetectorRef = inject(ChangeDetectorRef);
+  commentArr: any = []; */
   commentArr: any = signal([]);
 
   ngOnInit() {
     this.commentService.getAllComments().subscribe((response: any) => {
       this.commentArr.set(response);
+      /* this.commentArr = response;
+      this.changeDetectorRef.markForCheck(); */
     })
   }
 }
