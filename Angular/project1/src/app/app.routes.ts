6e75235ae7dc0027cb0f720a96a529e5f38f0aea
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-02-06 22:05:11 +0530

    recording

--- Angular/project1/src/app/app.routes.ts
+++ Angular/project1/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { Home } from './components/home/home';
 import { Routes } from '@angular/router';
 import { Notfound } from './components/notfound/notfound';
 import { UserList } from './components/user-list/user-list';
-import { ProductList } from './components/product-list/product-list';
+// import { ProductList } from './components/product-list/product-list';
 import { Userdetails } from './components/userdetails/userdetails';
 import { Productdetails } from './components/productdetails/productdetails';
 import { Permanentjobs } from './components/permanentjobs/permanentjobs';
@@ -32,7 +32,10 @@ export const routes: Routes = [
   },
   { path: 'users', component: UserList },
   { path: 'userdetails/:id', component: Userdetails },
-  { path: 'products', component: ProductList },
+  {
+    path: 'products',
+    loadComponent: () => import('./components/product-list/product-list').then((x) => x.ProductList)
+  },
   { path: 'productdetails', component: Productdetails },
   {
     path: 'uploadvideos',
